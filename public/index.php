312477e22ec08c50ffcd5493cb76381e74412968
John Francis <johnfrancis13@uchicago.edu>
2024-08-20 14:26:29 +0100

    again

--- public/index.php
+++ public/index.php
@@ -7,24 +7,35 @@ echo "Hello World!";
 // Include the database connection file
 require '../src/db/connection.php';
 
-$sql = "SELECT TOP 10 id, response,qnum,mcq FROM dbo.test_annotations_draft WHERE annotators < 3 ORDER BY NEWID()";
-$stmt = sqlsrv_query($conn, $sql);
+// $sql = "SELECT TOP 10 id, response,qnum,mcq FROM dbo.test_annotations_draft WHERE annotators < 3 ORDER BY NEWID()";
+// $stmt = sqlsrv_query($conn, $sql);
 
-if ($stmt === false) {
-    die(print_r(sqlsrv_errors(), true));
+// if ($stmt === false) {
+//     die(print_r(sqlsrv_errors(), true));
+// }
+
+// // Fetch and store the results in an array
+// $data = array();
+// while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+//     $data[] = $row;
+// }
+
+
+$tsql= "SELECT TOP 10 id, response,qnum,mcq FROM dbo.test_annotations_draft WHERE annotators < 3 ORDER BY NEWID()";
+$getResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table" . PHP_EOL);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['id'] . " " . $row['response'] . PHP_EOL);
 }
+sqlsrv_free_stmt($getResults);
 
-// Fetch and store the results in an array
-$data = array();
-while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-    $data[] = $row;
-}
+// // Encode the array to JSON and output it
+// header('Content-Type: application/json');
+// echo json_encode($data);
 
-// Encode the array to JSON and output it
-header('Content-Type: application/json');
-echo json_encode($data);
-
-// Free statement and close connection
-sqlsrv_free_stmt($stmt);
-sqlsrv_close($conn);
+// // Free statement and close connection
+// sqlsrv_free_stmt($stmt);
+// sqlsrv_close($conn);
 ?>
--- src/db/connection.php
+++ src/db/connection.php
@@ -1,41 +1,19 @@
 <?php
-// $servername = "mimde.database.windows.net";
-// $username = "ai4ps_mimde";
-// $password = "what_is_yasp_123";
-// $dbname = "mimde_qualtrics";
-
-// $serverName = "mimde.database.windows.net"; // update me
-// $connectionOptions = array(
-//     "Database" => "mimde_qualtrics", // update me
-//     "Uid" => "ai4ps_mimde", // update me
-//     "PWD" => "your_paswhat_is_yasp_123sword" // update me
-// );
-
-// //Establishes the connection
-// $conn = sqlsrv_connect($serverName, $connectionOptions);
-
-// // Check connection
-// if (mysqli_connect_errno()) {
-//     die('Failed to connect to MySQL: ' . mysqli_connect_error());
-// }
-
-// if ($conn === false) {
-//     die(print_r(sqlsrv_errors(), true));
-// }
-
-
-$host = 'mimde.database.windows.net';
-$username = 'ai4ps_mimde';
-$password = 'what_is_yasp_123';
-$db_name = 'mimde_qualtrics';
-
+$serverName = "mimde.database.windows.net"; // update me
+$connectionOptions = array(
+    "Database" => "mimde_qualtrics", // update me
+    "Uid" => "ai4ps_mimde", // update me
+    "PWD" => "what_is_yasp_123" // update me
+);
 //Establishes the connection
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+// Check connection
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-echo "Connected successfully";
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
 
 ?>
