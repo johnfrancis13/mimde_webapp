96d050610f2b4d6880e2edfa7d54890205cad735
John Francis <johnfrancis13@uchicago.edu>
2024-08-20 14:37:11 +0100

    clean up code

--- public/index.php
+++ public/index.php
@@ -2,24 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "Hello World!";
+header('Content-Type: application/json');
 
 // Include the database connection file
 require '../src/db/connection.php';
-
-// $sql = "SELECT TOP 10 id, response,qnum,mcq FROM dbo.test_annotations_draft WHERE annotators < 3 ORDER BY NEWID()";
-// $stmt = sqlsrv_query($conn, $sql);
-
-// if ($stmt === false) {
-//     die(print_r(sqlsrv_errors(), true));
-// }
-
-// // Fetch and store the results in an array
-// $data = array();
-// while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-//     $data[] = $row;
-// }
-
 
 $tsql= "SELECT TOP 10 id, response,qnum,mcq FROM dbo.test_annotations_draft WHERE annotators < 3 ORDER BY NEWID()";
 $getResults= sqlsrv_query($conn, $tsql);
@@ -37,17 +23,9 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 
 
 // Encode the array to JSON and output it
-header('Content-Type: application/json');
 echo json_encode($data);
 
 sqlsrv_free_stmt($getResults);
-qlsrv_close($conn);
+sqlsrv_close($conn);
 
-// // Encode the array to JSON and output it
-// header('Content-Type: application/json');
-// echo json_encode($data);
-
-// // Free statement and close connection
-// sqlsrv_free_stmt($stmt);
-// sqlsrv_close($conn);
 ?>
--- src/db/connection.php
+++ src/db/connection.php
@@ -5,8 +5,10 @@ $connectionOptions = array(
     "Uid" => "ai4ps_mimde", // update me
     "PWD" => "what_is_yasp_123" // update me
 );
+
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
+
 // Check connection
 if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
