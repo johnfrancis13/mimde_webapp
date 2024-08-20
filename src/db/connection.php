41d4afaf26a99234343c4e8cf1a2f5eb2258e00d
John Francis <johnfrancis13@uchicago.edu>
2024-08-20 14:17:29 +0100

    change connection

--- src/db/connection.php
+++ src/db/connection.php
@@ -1,25 +1,39 @@
 <?php
-$servername = "mimde.database.windows.net";
-$username = "ai4ps_mimde";
-$password = "what_is_yasp_123";
-$dbname = "mimde_qualtrics";
+// $servername = "mimde.database.windows.net";
+// $username = "ai4ps_mimde";
+// $password = "what_is_yasp_123";
+// $dbname = "mimde_qualtrics";
 
-$serverName = "mimde.database.windows.net"; // update me
-$connectionOptions = array(
-    "Database" => "mimde_qualtrics", // update me
-    "Uid" => "ai4ps_mimde", // update me
-    "PWD" => "your_paswhat_is_yasp_123sword" // update me
-);
+// $serverName = "mimde.database.windows.net"; // update me
+// $connectionOptions = array(
+//     "Database" => "mimde_qualtrics", // update me
+//     "Uid" => "ai4ps_mimde", // update me
+//     "PWD" => "your_paswhat_is_yasp_123sword" // update me
+// );
+
+// //Establishes the connection
+// $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// // Check connection
+// if (mysqli_connect_errno()) {
+//     die('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
+
+// if ($conn === false) {
+//     die(print_r(sqlsrv_errors(), true));
+// }
+
+
+$host = 'mimde.database.windows.net';
+$username = 'ai4ps_mimde';
+$password = 'what_is_yasp_123';
+$db_name = 'mimde_qualtrics';
+
 //Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-// Check connection
-if (mysqli_connect_errno()) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 echo "Connected successfully";
